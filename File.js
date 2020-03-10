ad66124c258cd40145d537fa12c2c85a66f8d14f
Hayden Bronson <Hayden.Bronson@mths.ca>
2020-03-10 13:01:49 +0000

    added index.html

---
+++ File.js
@@ -0,0 +1,6 @@
+function change () {
+    var image = document.getElementById('helloworld')
+    image.src = "./images/Earth.png";}
+function coloring () {
+    var str = "Hello, World!";
+    document.getElementById("color").innerHTML = str;}
